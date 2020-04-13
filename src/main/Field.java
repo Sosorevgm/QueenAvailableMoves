d7a0b043e970bdc33c9a044ea74663d2838d3279
Sosorevgm <Sosorevgm@gmail.com>
2020-04-13 13:40:03 +0300

    Add comments

--- src/main/Field.java
+++ src/main/Field.java
@@ -45,12 +45,18 @@ public class Field {
         }
 
 
-        // рисую доступные ходы по диагонали слева на право
+        // Рисую доступные ходы по диагонали слева на право.
+        // Возможное поле (leftStartPositionI) для первого цикла равна модулю разницы
+        // позиции королевы по горизонтали и позиции королевы по вертикали
         int leftStartPositionI = Math.abs(queenPositionB - queenPositionA);
+        // Если позиция королевы по горизонтали меньше позиции королевы по вертикали
+        // возможное поле (leftStartPositionJ) для второго цикла равна нулю
         if (queenPositionB < queenPositionA) {
             int leftStartPositionJ = 0;
             for (int i = 0; i < gameField.length; i++) {
                 for (int j = 0; j < gameField.length; j++) {
+                    // При совпадении возможных полей по горизонтали и вертикали (leftStartPositionI и leftStartPositionJ)
+                    // с итерируемыми переменнами в циклах рисую доступный ход и увеличиваю значение возможного поля на один
                     if (i == leftStartPositionI & j == leftStartPositionJ) {
                         gameField[i][j] = "-";
                         leftStartPositionI++;
@@ -59,10 +65,15 @@ public class Field {
                 }
             }
         } else {
+            // Если позиция корлевы по горизонтали больше или равна позиции королевы по вертикали
+            // возможная позиция (leftStartPositionJ) для для второго цикла равна их разнице
             int leftStartPositionJ = Math.abs(queenPositionB - queenPositionA);
             for (int i = 0; i < gameField.length; i++) {
                 for (int j = 0; j < gameField.length; j++) {
                     if (j == leftStartPositionJ) {
+                        // При совпадении возможной позиции (leftStartPositionJ) с итерируемой переменной
+                        // рисую доступный ход, увеличиваю значение возможного хода на один,
+                        // выхожу из второго цикла и перехожу к следующей горизонтали
                         gameField[i][j] = "-";
                         leftStartPositionJ++;
                         break;
@@ -71,16 +82,26 @@ public class Field {
             }
         }
 
-        // рисую доступные ходы по диагонали справа на лево
+        // Рисую доступные ходы по диагонали справа на лево.
+        // Если сумма позиций королевы по вертикали и горизонтали меньше или равна максимальному индексу в массиве,
+        // возможная позиция для первого цикла (rightStartPositionI) равна нулю.
+        // Возможная позиция для второго цикла (rightStartPositionJ) равна сумме
+        // значений позиции королевы по вертикали и горизонтали
         if (queenPositionA + queenPositionB <= 7) {
             int rightStartPositionI = 0;
             int rightStartPositionJ = queenPositionA + queenPositionB;
+            // Если значение возможной позиции для второго цикла (rightStartPositionJ)
+            // превышает максимальный размер массива, задаю ему максимально возможное значение
             if (rightStartPositionJ > 7) {
                 rightStartPositionJ = 7;
             }
             for (int i = 0; i < gameField.length; i++) {
                 for (int j = 0; j < gameField.length; j++) {
                     if (i == rightStartPositionI & j == rightStartPositionJ) {
+                        // При совпадении возможных полей по горизонтали и вертикали (rightStartPositionI и rightStartPositionJ)
+                        // с итерируемыми переменнами в циклах рисую доступный ход,
+                        // значение возможного поля для первого цикла (rightStartPositionI) увеличивается,
+                        // значение возможного поля для второго цикла (rightStartPositionJ) уменьшается.
                         gameField[i][j] = "-";
                         rightStartPositionI++;
                         rightStartPositionJ--;
@@ -88,11 +109,19 @@ public class Field {
                 }
             }
         } else {
+            // Если сумма позиций королевы по вертикали и горизонтали превышает максимальный индекс в массиве,
+            // возможная позиция для первого цикла (rightStartPositionI) равна
+            // сумме позиций королевы по вертикали и горизонтали минус максимальный индекс массива.
+            // Возможная позиция для второго цикла (rightStartPositionJ) равна максимальному индексу массива.
             int rightStartPositionI = queenPositionA + queenPositionB - 7;
             int rightStartPositionJ = 7;
             for (int i = 0; i < gameField.length; i++) {
                 for (int j = 0; j < gameField.length; j++) {
                     if (i == rightStartPositionI & j == rightStartPositionJ) {
+                        // При совпадении возможных полей по горизонтали и вертикали (rightStartPositionI и rightStartPositionJ)
+                        // с итерируемыми переменнами в циклах рисую доступный ход,
+                        // значение возможного поля для первого цикла (rightStartPositionI) увеличивается,
+                        // значение возможного поля для второго цикла (rightStartPositionJ) уменьшается.
                         gameField[i][j] = "-";
                         rightStartPositionI++;
                         rightStartPositionJ--;
@@ -100,6 +129,7 @@ public class Field {
                 }
             }
         }
+        // Рисую позицию королевы на поле
         gameField[queenPositionA][queenPositionB] = "Q";
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Field field = new Field();
         field.fillField();
-        field.putQueen(3,4);
+        field.putQueen(7,4);
         field.showAvailableMoves();
         field.printField();
     }
